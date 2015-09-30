2f42421b1c59a8c26161456951502d566e865215
sergit <storrellas@gmail.com>
2015-09-30 13:35:14 +0200

    Added templates

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def dashboard(request):
-    return HttpResponse("return this string")
+    #return HttpResponse("return this string")
+    return render(request, "dashboard.html")
