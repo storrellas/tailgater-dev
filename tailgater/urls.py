fff2925cb632dcf99066c11670fa416a2ae70652
sergit <storrellas@gmail.com>
2015-09-30 14:33:52 +0200

    Modified urls to add home

--- tailgater/urls.py
+++ tailgater/urls.py
@@ -9,4 +9,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     
     url(r'^dashboard/', include('dashboard.urls')),
+    
+    url(r'^$', include('dashboard.urls')),
 )
